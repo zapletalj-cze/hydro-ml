d2fe8f5f7c2dffca14a5657bfbd2cb79d0a5a6f0
Jakub Zapletal <jakub.zapletal@aon.com>
2026-04-07 13:12:12 +0200

    mosaic rasters to be fixed

---
+++ 05_mosaic_rasters.py
@@ -0,0 +1,73 @@
+from ifgis.raster import MosaicRasters
+import geopandas as gpd
+from gis import Vector, Raster
+from osgeo import gdal, osr
+
+TILEINDEX_ROOT = r"D:\90_PersonalFoldlers\JZa\DataProcessing\levees_detection\sentinel\01_data\sentinel1_data\processed_selected"
+AOI = r"D:\90_PersonalFoldlers\JZa\DataProcessing\levees_detection\sentinel\01_data\AOI_Poland.gpkg"
+SNAP_RASTER = r"B:\01_Projects\154_Poland_Flood_v3\01_MD\01_HAZARD\00_GIS\01_SnapRaster\SnapRaster_PL_10m_2180.tif"
+files = ["asc_VH_tile_index.gpkg", "asc_VV_tile_index.gpkg", "desc_VH_tile_index.gpkg", "desc_VV_tile_index.gpkg"]
+CELL_SIZE = 10
+EPSG = 2180
+
+gdf_aoi = gpd.read_file(AOI)
+bounds = gdf_aoi.total_bounds
+extent_aoi = bounds[0], bounds[2], bounds[1], bounds[3]
+extent_aoi_snapped = Vector.align_extent_to_snap(extent_aoi, SNAP_RASTER)
+nodata = -84544
+for file in files:
+    tileindex_path = f"{TILEINDEX_ROOT}/{file}"
+    gdf_tileindex = gpd.read_file(tileindex_path)
+    if nodata == -84544:
+        nodata = Raster.get_raster_nodata(gdf_tileindex["path"].iloc[0])
+    groups = gdf_tileindex["group"].unique().tolist()
+    for group in groups:
+        gdf_group = gdf_tileindex[gdf_tileindex["group"] == group]
+        asc_desc = "asc" if "asc" in file else "desc"
+        specifics = "VH" if "VH" in file else "VV"
+        outfilename = f"{group}_{asc_desc}_{specifics}_sigma0-elp.tif"
+        rasters_group = gdf_group["path"].tolist()
+        warped_rasters = []
+        for idx, raster in enumerate(rasters_group):
+            print(f"Processing raster: {raster}")
+            # Open raster to get properties
+            ds_in = gdal.Open(raster)
+            if ds_in is None:
+                raise FileNotFoundError(f"Could not open raster: {raster}")
+            
+            # Get raster information
+            raster_nodata = ds_in.GetRasterBand(1).GetNoDataValue()
+            data_type = ds_in.GetRasterBand(1).DataType
+            srs_in = osr.SpatialReference()
+            srs_in.ImportFromWkt(ds_in.GetProjection())
+            
+            srs_out = osr.SpatialReference()
+            srs_out.ImportFromEPSG(EPSG)
+            
+            # Create vsimem output path
+            vsimem_path = f"/vsimem/warped_{group}_{asc_desc}_{specifics}_{idx}.tif"
+            
+            # Prepare bounds: convert (xmin, xmax, ymin, ymax) to (minx, miny, maxx, maxy)
+            bounds = (extent_aoi_snapped[0], extent_aoi_snapped[2], 
+                     extent_aoi_snapped[1], extent_aoi_snapped[3])
+            
+            # Clip by AOI and snap to extent bounds
+            warp_options = gdal.WarpOptions(
+                format="GTiff",
+                cutlineDSName=AOI,
+                xRes=CELL_SIZE,
+                yRes=CELL_SIZE,
+                dstSRS=srs_out,
+                srcSRS=srs_in,
+                outputBounds=bounds,
+                srcNodata=raster_nodata,
+                dstNodata=nodata,
+                outputType=data_type
+            )
+            gdal.Warp(vsimem_path, ds_in, options=warp_options)
+            ds_in = None
+            
+            warped_rasters.append(vsimem_path)
+
+        outpath = f"{TILEINDEX_ROOT}/{outfilename}"
+        mosaic_rasters = MosaicRasters(warped_rasters, output=outpath, cell_size=CELL_SIZE, epsg=EPSG, data_type=gdal.GDT_Float32, no_data=nodata, extent=extent_aoi_snapped).mosaic_in_order()
